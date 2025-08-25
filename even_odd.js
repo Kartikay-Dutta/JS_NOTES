91aa6a5ddf63660abfcdaa4d22a7e4add62bf6a9
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-25 10:12:45 +0530

    code\

---
+++ even_odd.js
@@ -0,0 +1,11 @@
+// evenOdd.js
+
+// Take a number as input (example: 7)
+let number = 7;
+
+// Check even or odd
+if (number % 2 === 0) {
+    console.log(`${number} is Even`);
+} else {
+    console.log(`${number} is Odd`);
+}
